076a1488fabf45f717b8493da6d96bb9a9837709
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2021-08-06 19:33:12 +0200

    init commit

---
+++ app/app.py
@@ -0,0 +1,43 @@
+#!/bin/env python
+import os
+import pymongo
+import json
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+app.app_context().push()
+
+myclient = pymongo.MongoClient("mongodb://mongo_server:27017")
+
+ENDPOINT_MESSAGE = os.environ.get('ENDPOINT_MESSAGE')
+
+
+@app.route('/')
+def hello_world():
+    return '{0} {1}'.format("MESSAGE:", ENDPOINT_MESSAGE)
+
+
+@app.route('/user')
+def user():
+   return 'user'
+
+
+@app.route("/<db>/<collection>", methods=['GET'])
+def get_all_users(db, collection):
+    pymongo_cursor = myclient[db][collection].find()
+    data = []
+    for doc in pymongo_cursor:
+        doc['_id'] = str(doc['_id'])
+        data.append(doc)
+    return jsonify(data)
+
+
+@app.route("/<db>/<collection>/<id>", methods=['GET'])
+def get_user_by_id(db, collection, id):
+    pymongo_cursor = myclient[db][collection].find({"id":id})
+    data = []
+    for doc in pymongo_cursor:
+        doc['_id'] = str(doc['_id'])
+        data.append(doc)
+    return jsonify(data)
